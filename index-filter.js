2f34b7b0dd8b19a8e39c7fffb02ab8d768a8a1dd
jodywhitesides <93063182+jodywhitesides@users.noreply.github.com>
2025-04-01 21:55:11 -0600

    Delete index-filter.js
    switching gears

--- index-filter.js
+++
@@ -1,8 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import Edit from './edit-filter.js';
-import metadata from './block.json';
-
-registerBlockType(metadata.name, {
-    edit: Edit,
-    save: () => null // Uses server-side rendering
-});
