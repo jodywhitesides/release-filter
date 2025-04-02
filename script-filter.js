ea3ccc0addeee83b31ad0156d1fb6b7762064267
jodywhitesides <93063182+jodywhitesides@users.noreply.github.com>
2025-04-01 21:55:23 -0600

    Delete script-filter.js
    switching gears

--- script-filter.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdown = document.querySelector(".release-filter-dropdown");
-    if (dropdown) {
-        dropdown.addEventListener("change", function () {
-            this.form.submit(); // Reloads page automatically when selection changes
-        });
-    }
-});
